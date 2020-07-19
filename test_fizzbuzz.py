7fa0c301ed6c2c17ecdda65c0e98f38370e26c4a
menasheZ <67259121+menasheZ@users.noreply.github.com>
2020-07-19 15:16:40 +0300

    remove catch

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,31 +3,31 @@ import fbTDD
 
 # check if Prime
 def test_if_prime_number():
-    assert fbTDD.isPrimeNumber(5) == "Prime"
+    assert fbTDD.is_prime_number(5) == "Prime"
 
 
 # check if not prime
 def test_not_prime_number():
-    assert fbTDD.isPrimeNumber(6) == ""
+    assert fbTDD.is_prime_number(6) == ""
 
 
 # check if return Number
 def test_return_num():
-    assert fbTDD.fizzbuzz(11) == "11"
+    assert fbTDD.fizz_buzz(11) == "11"
 
 
 # check DIv15
 def test_check15():
-    assert fbTDD.fizzbuzz(30) == "FizzBuzz"
+    assert fbTDD.fizz_buzz(30) == "FizzBuzz"
 
 
 # check DIv5
 def test_check5():
-    assert fbTDD.fizzbuzz(10) == "Fizz"
+    assert fbTDD.fizz_buzz(10) == "Fizz"
 
 
 # check DIv3
 def test_check3():
-    assert fbTDD.fizzbuzz(3) == "Buzz"
+    assert fbTDD.fizz_buzz(3) == "Buzz"
 
 
